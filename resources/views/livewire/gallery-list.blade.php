aa644558f05e39f07db7403dfa322454f64f2a04
Mortreddd <emmanmale@gmail.com>
2025-05-18 19:23:09 +0800

    fix: Fixed issue regarding assets not showing when added on production

--- resources/views/livewire/gallery-list.blade.php
+++ resources/views/livewire/gallery-list.blade.php
@@ -6,7 +6,7 @@
     <h1 class="block text-center text-gray-800 font-sans font-bold text-4xl mb-10">Gallery</h1>
     <div class="w-full relative flex gap-3 flex-wrap justify-center">
         @forelse ($galleries as $gallery)
-        <img src="{{ asset($gallery->image) }}" alt="Gallery Image"
+        <img src="{{ asset('storage/'.$gallery->image) }}" alt="Gallery Image"
             class="object-contain h-72 max-w-96 object-center mask-add rounded-lg" />
         @empty
         <p>No images found.</p>
