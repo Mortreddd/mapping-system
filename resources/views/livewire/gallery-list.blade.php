ae461dd455bd2ab80f9b04060854125f28221407
Mortreddd <emmanmale@gmail.com>
2025-05-19 10:41:07 +0800

    fix: issue regarding html tag in livewire

--- resources/views/livewire/gallery-list.blade.php
+++ resources/views/livewire/gallery-list.blade.php
@@ -1,16 +1,14 @@
 {{-- Do your work, then step back. --}}
 
 
-@if($galleries->isNotEmpty())
 <section class="w-full bg-white py-5 my-10 px-20 flex flex-col items-center">
     <h1 class="block text-center text-gray-800 font-sans font-bold text-4xl mb-10">Gallery</h1>
     <div class="w-full relative flex gap-3 flex-wrap justify-center">
         @forelse ($galleries as $gallery)
-        <img src="{{ asset($gallery->image) }}" alt="Gallery Image"
+        <img src="{{ asset('storage/'.$gallery->image) }}" alt="Gallery Image"
             class="object-contain h-72 max-w-96 object-center mask-add rounded-lg" />
         @empty
         <p>No images found.</p>
         @endforelse
     </div>
 </section>
-@endif
